592ecb626528b35d6c78d22935f5f73420da50cb
kk <kevin197011@outlook.com>
2024-11-20 09:59:25 +0000

    Update 2024-11-20 09:59:25 +0000.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bolt
 bolt
+
+bolt run command 'sudo uptime' --targets devops
+bolt plan run bolt::deploy --targets devops
--- plans/deploy.pp
+++ plans/deploy.pp
@@ -1,6 +1,9 @@
 plan bolt::deploy (
   TargetSpec $nodes
 ) {
-  run_command('date', $nodes)
-  run_command('uptime', $nodes)
+  $date_result = run_command('date', $nodes)
+  out::message($date_result)
+
+  $uptime_result = run_command('uptime', $nodes)
+  out::message($uptime_result)
 }
